69f737f19ccf439e0596098ec465a41a6655090f
arnage <arnage92@gmail.com>
2016-12-20 23:28:50 +0300

    Update api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 	root to: 'index#index'
-  	post '/sign_in', to: 'auth#authenticate_user'
-  	post '/sign_up', to: 'auth#register_user'
+  	post '/api/sign_in', to: 'auth#authenticate_user'
+  	post '/api/sign_up', to: 'auth#register_user'
 end
